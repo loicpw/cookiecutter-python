18422113ce2091597f94312ccdb1e7840db0a8fd
Loïc Peron <peronloic.us@gmail.com>
2018-09-28 16:15:50 -0600

    use pathlib in pep8 test, make it more flexible

--- {{cookiecutter.package_name}}/tests/test_pep8.py
+++ {{cookiecutter.package_name}}/tests/test_pep8.py
@@ -2,12 +2,14 @@
 
 import {{ cookiecutter.package_name }}
 import pep8
-import os.path
+import pathlib
 
 def test_pep8_conformance():
     """Test that we conform to PEP8."""
     pep8style = pep8.StyleGuide(prefix='E')
-    result = pep8style.check_files([
-        os.path.dirname({{ cookiecutter.package_name }}.__file__),
-    ])
+    base = pathlib.Path({{ cookiecutter.package_name }}.__file__).parent
+    result = pep8style.check_files(map(str, [
+        base,
+        # base / 'subdir',
+    ]))
     assert result.total_errors == 0
